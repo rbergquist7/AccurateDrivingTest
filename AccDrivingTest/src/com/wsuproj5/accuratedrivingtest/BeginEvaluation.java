cc9643ffe6658e29cea72eb43a978089f9e8cca0
Ryan RyanBergquist Bergquist <r.bergquist7@gmail.com>
2015-02-23 00:13:44 -0800

    changed some files

--- AccDrivingTest/src/com/wsuproj5/accuratedrivingtest/BeginEvaluation.java
+++ AccDrivingTest/src/com/wsuproj5/accuratedrivingtest/BeginEvaluation.java
@@ -1,432 +1,82 @@
 package com.wsuproj5.accuratedrivingtest;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+/*OBD connection will be done at beginning of during Evaluation and exit 
+ * to main menu if unconnected. this will save the challenge of trying to pass
+ * the object that holds the bluetooth connection. 
+ */
 
+public class BeginEvaluation extends ActionBarActivity{
+	protected void onCreate(Bundle savedInstanceState) {
+		
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.activity_begin_evaluation);
+	      fillRoutes();
+	      fillTests();
+	      fillEvaluationsName();
+		   }
+		   
+		   private void fillEvaluationsName() {
+		/*evaluators name is a text field, so get Evaluators name from login screen
+		 * and fill the text field after variable is set
+		 */
+	}
+		   private void getDriversName(){
+			   /*get drivers name from the text field*/
+		   }
+		   private void toDuringEvaluation(View view){
+			   /*check if all fields have been filled
+			    * check obd connection onCreate of duringEvaluation()
+			    */
+		   }
 
-import de.greenrobot.event.EventBus;
+		private void fillRoutes() {
+				SharedPreferences prefs = getSharedPreferences("existingRoutes", Context.MODE_PRIVATE);
+				List<String> spinnerArray =  new ArrayList<String>();
+				for (int i = 0; i < 1000; i++) {
+					String route = prefs.getString("route" + i, null);
+					if (route == null || route.equals("removed")) {
+						break;
+					}
+					spinnerArray.add("route " + i);
+				}
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				    this, android.R.layout.simple_spinner_item, spinnerArray);
 
+				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				Spinner sItems = (Spinner) findViewById(R.id.route_spinner1);
+				sItems.setAdapter(adapter);
+			}
+		   
+		   private void fillTests() {
+				SharedPreferences prefs = getSharedPreferences("existingRoutes", Context.MODE_PRIVATE);
+				List<String> spinnerArray =  new ArrayList<String>();
+				for (int i = 0; i < 1000; i++) {
+					String test = prefs.getString("test" + i, null);
+					if (test == null || test.equals("removed")) {
+						break;
+					}
+					spinnerArray.add("test " + i);
+				}
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				    this, android.R.layout.simple_spinner_item, spinnerArray);
 
-public class BeginEvaluation extends ActionBarActivity implements
-        PairedDevicesDialog.PairedDeviceDialogListener
-{
-    private static final String TAG = BeginEvaluation.class.getSimpleName();
-    private static final String TAG_DIALOG = "dialog";
-    private static final String NO_BLUETOOTH = "Oops, your device doesn't support bluetooth";
-  
-    // Intent request codes
-    private static final int REQUEST_ENABLE_BT = 101;
- 
-    // Message types accessed from the BluetoothIOGateway Handler
-    public static final int MESSAGE_STATE_CHANGE = 1;
-    public static final int MESSAGE_READ = 2;
-    public static final int MESSAGE_WRITE = 3;
-    public static final int MESSAGE_DEVICE_NAME = 4;
-    public static final int MESSAGE_TOAST = 5;
-
-    // Key names accesses from the BluetoothIOGateway Handler
-    public static final String DEVICE_NAME = "device_name";
-    public static final String TOAST = "toast_message";
-
-    // Bluetooth
-    private BluetoothIOGateway mIOGateway;
-    private static BluetoothAdapter mBluetoothAdapter;
-    private DeviceBroadcastReceiver mReceiver;
-    private PairedDevicesDialog dialog;
-    private List<BluetoothDevice> mDeviceList;
-
-    // Widgets
-    private TextView mConnectionStatus;
-    private TextView mMonitor;
-    private ImageButton mBtnSend;
-    
-    // Variable def
-    private static StringBuilder mSbCmdResp;
-    private String mConnectedDeviceName;
-    private final Handler mMsgHandler = new Handler()
-    {
-        @Override
-        public void handleMessage(Message msg)
-        {
-            switch (msg.what)
-            {
-                case MESSAGE_STATE_CHANGE:
-                    switch (msg.arg1)
-                    {
-                        case BluetoothIOGateway.STATE_CONNECTING:
-                            mConnectionStatus.setText(getString(R.string.BT_connecting));
-                            mConnectionStatus.setBackgroundColor(Color.YELLOW);
-                            break;
-
-                        case BluetoothIOGateway.STATE_CONNECTED:
-                            mBtnSend.setEnabled(true);
-                            mConnectionStatus.setText(getString(R.string.BT_status_connected_to) + " " + mConnectedDeviceName);
-                            mConnectionStatus.setBackgroundColor(Color.GREEN);
-                            break;
-
-                        case BluetoothIOGateway.STATE_LISTEN:
-                        case BluetoothIOGateway.STATE_NONE:
-                            mBtnSend.setEnabled(false);
-                            mConnectionStatus.setText(getString(R.string.BT_status_not_connected));
-                            mConnectionStatus.setBackgroundColor(Color.RED);
-                            break;
-
-                        default:
-                            break;
-                    }
-                    break;
-
-               
-                case MESSAGE_TOAST:
-                    displayMessage(msg.getData().getString(TOAST));
-                    break;
-
-                case MESSAGE_DEVICE_NAME:
-                    // save the connected device's name
-                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
-                    break;
-            }
-        }
-
-    };
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        setContentView(R.layout.activity_elm_main);
-
-        // log
-        displayLog("=>\n***************\n     ELM 327 started\n***************");
-
-        // connect widgets
-        mMonitor = (TextView) findViewById(R.id.tvMonitor);
-        mMonitor.setMovementMethod(new ScrollingMovementMethod());
-        mConnectionStatus = (TextView) findViewById(R.id.tvConnectionStatus);
-       
-
-        // make sure user has Bluetooth hardware
-        displayLog("Try to check hardware...");
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null)
-        {
-            // Device does not support Bluetooth
-            displayMessage(NO_BLUETOOTH);
-            displayLog(NO_BLUETOOTH);
-            
-            BeginEvaluation.this.finish();
-        }
-        // log
-        displayLog("Bluetooth found.");
-        
-        // Init variables
-        mSbCmdResp = new StringBuilder();
-        mIOGateway = new BluetoothIOGateway(this, mMsgHandler);
-    }
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-
-        if (mBluetoothAdapter == null)
-        {
-            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        }
-
-        // make sure Bluetooth is enabled
-        displayLog("Try to check availability...");
-        if (!mBluetoothAdapter.isEnabled())
-        {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-        else
-        {
-            displayLog("Bluetooth is available");
-
-            queryPairedDevices();
-            setupMonitor();
-        }
-    }
-
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-
-        // Register EventBus
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-
-        // Unregister EventBus
-        EventBus.getDefault().unregister(this);
-    }
-
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
-
-        // Un register receiver
-        if (mReceiver != null)
-        {
-            unregisterReceiver(mReceiver);
-        }
-
-        // Stop scanning if is in progress
-        cancelScanning();
-
-        // Stop mIOGateway
-        if (mIOGateway != null)
-        {
-            mIOGateway.stop();
-        }
-        
-        // Clear StringBuilder
-        if (mSbCmdResp.length() > 0)
-        {
-            mSbCmdResp.setLength(0);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-    	if(item.getItemId() == R.id.action_scan){
-    		queryPairedDevices();
-                setupMonitor();
-                return true;
-    	}
-    	
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-
-
-        switch (requestCode)
-        {
-            case REQUEST_ENABLE_BT:
-                if (resultCode == RESULT_CANCELED)
-                {
-                    displayMessage("Bluetooth not enabled :(");
-                    displayLog("Bluetooth not enabled :(");
-                    return;
-                }
-
-                if (resultCode == RESULT_OK)
-                {
-                    displayLog("Bluetooth enabled");
-
-                    queryPairedDevices();
-                    setupMonitor();
-                }
-
-                break;
-
-            default:
-                // nothing at the moment
-        }
-    }
-    
-    private void setupMonitor()
-    {
-        // Start mIOGateway
-        if (mIOGateway == null)
-        {
-            mIOGateway = new BluetoothIOGateway(this, mMsgHandler);
-        }
-
-        // Only if the state is STATE_NONE, do we know that we haven't started already
-        if (mIOGateway.getState() == BluetoothIOGateway.STATE_NONE)
-        {
-            // Start the Bluetooth chat services
-            mIOGateway.start();
-        }
-
-        // clear string builder if contains data
-        if (mSbCmdResp.length() > 0)
-        {
-            mSbCmdResp.setLength(0);
-        }
-        
-    }
-
-    private void queryPairedDevices()
-    {
-        displayLog("Try to query paired devices...");
-        
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0)
-        {
-            PairedDevicesDialog dialog = new PairedDevicesDialog();
-            dialog.setAdapter(new PairedListAdapter(this, pairedDevices), false);
-            showChooserDialog(dialog);
-        }
-        else
-        {
-            displayLog("No paired device found");
-
-            scanAroundDevices();
-        }
-    }
-
-    private void showChooserDialog(DialogFragment dialogFragment)
-    {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        Fragment prev = getSupportFragmentManager().findFragmentByTag(TAG_DIALOG);
-        if (prev != null)
-        {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        dialogFragment.show(ft, "dialog");
-    }
-
-    private void scanAroundDevices()
-    {
-        displayLog("Try to scan around devices...");
-
-        if (mReceiver == null)
-        {
-            // Register the BroadcastReceiver
-            mReceiver = new DeviceBroadcastReceiver();
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(mReceiver, filter);
-        }
-
-        // Start scanning
-        mBluetoothAdapter.startDiscovery();
-    }
-
-    private void cancelScanning()
-    {        
-        if (mBluetoothAdapter.isDiscovering())
-        {
-            mBluetoothAdapter.cancelDiscovery();
-
-            displayLog("Scanning canceled.");
-        }
-    }
-
-   
-    public void onEvent(BluetoothDevice device)
-    {
-        if (mDeviceList == null)
-        {
-            mDeviceList = new ArrayList<BluetoothDevice>(10);
-        }
-
-        mDeviceList.add(device);
-
-        // create dialog
-        final Fragment fragment = this.getSupportFragmentManager().findFragmentByTag(TAG_DIALOG);
-        if (fragment != null && fragment instanceof PairedDevicesDialog)
-        {
-            PairedListAdapter adapter = dialog.getAdapter();
-            adapter.notifyDataSetChanged();
-        }
-        else
-        {
-            dialog = new PairedDevicesDialog();
-            dialog.setAdapter(new PairedListAdapter(this, new HashSet<BluetoothDevice>(mDeviceList)), true);
-            showChooserDialog(dialog);
-        }
-    }
-
-    private void displayMessage(String msg)
-    {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
-    private void displayLog(String msg)
-    {
-        Log.d(TAG, msg);
-    }
-
-    @Override
-    public void onDeviceSelected(BluetoothDevice device)
-    {
-        cancelScanning();
-
-        displayLog("Selected device: " + device.getName() + " (" + device.getAddress() + ")");
-        
-        // Attempt to connect to the device
-        mIOGateway.connect(device, true);
-    }
-
-    @Override
-    public void onSearchAroundDevicesRequested()
-    {
-        scanAroundDevices();
-    }
-
-    @Override
-    public void onCancelScanningRequested()
-    {
-        cancelScanning();
-    }
-
-
+				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				Spinner sItems = (Spinner) findViewById(R.id.test_spinner);
+				sItems.setAdapter(adapter);
+			}
+		 
 }
 
-//show route selected
-//show tests selected
 
-/*until the backend is set up, have user enter driver and evaluator name*/
 
-//show evaluator
-//show driver
-
-//bundle settings
-//show obd connecion at beginning of during eval
